4a7ad2e0ad61d829c77d5e31c60ebefd55e2f90f
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-22 21:46:48 +0600

    run function call

--- index.js
+++ index.js
@@ -11,13 +11,16 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USSER}:${process.env.DB_PASS}@cluster0.qpwac.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  console.log('db connect');
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+  try{
 
+  }
+  finally{
+
+  }
+}
+
+run().catch(console.dir)
 app.get('/', (req, res) => {
     res.send('tools management')
   })
