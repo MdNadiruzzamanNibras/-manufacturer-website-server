7d76fe84693f981b36f7232cdb0bd0590c730736
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-22 21:37:29 +0600

    midle ware

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
 app.get('/', (req, res) => {
     res.send('tools management')
   })
