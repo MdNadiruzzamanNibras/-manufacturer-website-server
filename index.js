de99b8ef4c921ae2818cb6e7d042cf9f549f2869
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-22 21:44:08 +0600

    db connect

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USSER}:${process.env.DB_PASS}@cluster0.qpwac.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  console.log('db connect');
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.get('/', (req, res) => {
     res.send('tools management')
   })
