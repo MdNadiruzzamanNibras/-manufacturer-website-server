ef8c88b36fc4b7d202a586d84b6e42199dd873ee
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-24 16:08:58 +0600

    token set

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -15,6 +16,7 @@ async function run(){
     await client.connect();
     const toolCollection = client.db('ToolManagement').collection('tools');
     const ordersCollection = client.db('ToolManagement').collection('orders');
+    const makeAdminCollection = client.db('ToolManagement').collection('makeAdmins');
     app.get('/tools', async(req,res)=>{
       const qurey = {}
       const cursor =  toolCollection.find(qurey)
@@ -27,6 +29,26 @@ async function run(){
       const tool = await toolCollection.findOne(qurey)
       res.send(tool)
   })
+  app.put('/user/:email', async (req, res) => {
+    const email = req.params.email
+    const user = req.body
+    const filter = { email: email }
+    const options = { upsert: true };
+    const updateDoc = {
+      $set: user
+    };
+    const result = await makeAdminCollection.updateOne(filter, updateDoc, options)
+    const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN,{ expiresIn: '20d' })
+    res.send({result, token})
+
+  })
+   app.get('/order', async(req,res)=>{
+     const email = req.body.BuyerEmail
+     const qurey = {email : email}
+     const order = await ordersCollection.find(qurey).toArray()
+     res.send(order)
+
+   })
    app.post('/order', async(req,res)=>{
      const order= req.body
      const result = await ordersCollection.insertOne(order)
